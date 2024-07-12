1f4729587d69f133eda44fd7d202088d6a0fc18f
Shiqi Yuan <shiqi.yuan@rea-group.com>
2024-07-12 18:22:25 +0800

    modify structure and rename file

--- data-process/daily_data_analysis.py
+++ data-process/daily_data_analysis.py
@@ -15,7 +15,6 @@ df=spark.read.csv("s3a://47-data-test/data",header=True,inferSchema=True)
 df.createOrReplaceTempView("data_Q1_2019")
 df_result=spark.sql("select date,count(serial_number) as drive_count ,sum(failure) as drive_failures from data_Q1_2019 group by date order by date")
 
-#df_result.write.mode("overwrite").format("csv").option("header","true").save(f"/Users/shiqi.yuan/pyspark/answer1.csv")
 s3_destination_path = "s3a://47-data-test/daily-analysis.csv"
 df_result.write.mode("overwrite").format("csv").option("header","true").save(s3_destination_path)
 
